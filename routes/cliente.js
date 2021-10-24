2cb31e9d19c21b4ea526248083d4964685338cb4
JustinAMB <Justinmb@outlook.es>
2021-10-23 22:59:07 -0600

    se corrige el router de clientes

--- routes/cliente.js
+++ routes/cliente.js
@@ -1,5 +1,15 @@
 const express = require('express');
-const cliente = require('../controllers/cliente');
+const adminController = require('../controllers/AdminController');
+const auth = require('../middlewares/authenticate');
 const router = express.Router();
-router.post('/registro', cliente.registroCliente);
+
+router.post('/registro_admin', adminController.registro_admin);
+router.post('/login_admin', adminController.login_admin);
+
+router.get('/obtener_mensajes_admin', auth.auth, adminController.obtener_mensajes_admin);
+router.put('/cerrar_mensaje_admin/:id', auth.auth, adminController.cerrar_mensaje_admin);
+
+router.get('/obtener_ventas_admin/:desde?/:hasta?', auth.auth, adminController.obtener_ventas_admin);
+router.get('/kpi_ganancias_mensuales_admin', auth.auth, adminController.kpi_ganancias_mensuales_admin);
+
 module.exports = router;
