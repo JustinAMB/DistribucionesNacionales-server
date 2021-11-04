f7addb446f7f3d338cdce9534fc75776c0ca49a9
JustinAMB <Justinmb@outlook.es>
2021-11-03 23:59:15 -0600

    se realizan cambios en el router de admin

--- routes/admin.js
+++ routes/admin.js
@@ -1,15 +1,15 @@
 const express = require('express');
-const adminController = require('../controllers/admin');
+const admin = require('../controllers/admin');
 const auth = require('../middlewares/authenticate');
 const router = express.Router();
 
-router.post('/registro_admin', adminController.registro_admin);
-router.post('/login', adminController.login_admin);
+router.post('/registro_admin', admin.registro_admin);
+router.post('/login', admin.login_admin);
 
-router.get('/obtener_mensajes_admin', auth.auth, adminController.obtener_mensajes_admin);
-router.put('/cerrar_mensaje_admin/:id', auth.auth, adminController.cerrar_mensaje_admin);
+router.get('/obtener_mensajes_admin', auth.auth, admin.obtener_mensajes_admin);
+router.put('/cerrar_mensaje_admin/:id', auth.auth, admin.cerrar_mensaje_admin);
 
-router.get('/obtener_ventas_admin/:desde?/:hasta?', auth.auth, adminController.obtener_ventas_admin);
-router.get('/kpi_ganancias_mensuales_admin', auth.auth, adminController.kpi_ganancias_mensuales_admin);
+router.get('/obtener_ventas_admin/:desde?/:hasta?', auth.auth, admin.obtener_ventas_admin);
+router.get('/kpi_ganancias_mensuales_admin', auth.auth, admin.kpi_ganancias_mensuales_admin);
 
 module.exports = router;
