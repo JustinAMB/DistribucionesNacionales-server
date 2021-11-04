22bb526f9c481e9b0a4fdf5fd7b28ff8991c248b
JustinAMB <Justinmb@outlook.es>
2021-11-03 23:56:04 -0600

    se realizan correciones en el router de carrito

--- routes/carrito.js
+++ routes/carrito.js
@@ -1,11 +1,11 @@
 const express = require('express');
-const carritoController = require('../controllers/carritoController');
+const carrito = require('../controllers/carrito');
 
 const router = express.Router();
 const auth = require('../middlewares/authenticate');
 
-router.post('/agregar_carrito_cliente', auth.auth, carritoController.agregar_carrito_cliente);
-router.get('/obtener_carrito_cliente/:id', auth.auth, carritoController.obtener_carrito_cliente);
-router.delete('/eliminar_carrito_cliente/:id', auth.auth, carritoController.eliminar_carrito_cliente);
+router.post('/agregar_carrito_cliente', auth.auth, carrito.agregar_carrito_cliente);
+router.get('/obtener_carrito_cliente/:id', auth.auth, carrito.obtener_carrito_cliente);
+router.delete('/eliminar_carrito_cliente/:id', auth.auth, carrito.eliminar_carrito_cliente);
 
 module.exports = router;
